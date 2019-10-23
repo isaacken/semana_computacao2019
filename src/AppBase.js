aef36fabb26addb16bb329f753569024b681dabe
Isaac <isaackennedyfds@gmail.com>
2019-10-23 19:08:36 -0300

    Input form control

--- src/AppBase.js
+++ src/AppBase.js
@@ -11,7 +11,9 @@ export default class AppBase extends React.Component {
                 {name: "Bolacha"},
                 {name: "Batata"},
                 {name: "Biscoito"}
-            ]
+            ],
+            temp: 1,
+            content: 'conteudo'
         }
     }
 
@@ -23,12 +25,32 @@ export default class AppBase extends React.Component {
         });
     }
 
+    changeInput = (event) => {
+        return (e) => {
+            this.setState({
+                temp: e.target.value
+            });   
+        }
+    }
+
+    gogogo = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
     render() {
         return (
             <div className="AppBase">
                 <Lista type="circle">
                     { this.renderItems() }
                 </Lista>
+                <form onChange={this.gogogo}>
+                    <input name="temp" type="range" min="1" max="5" step="1" value={this.state.temp} /* onChange={this.changeInput("range")} */ />
+                    <p>Nota {this.state.temp}</p>
+                    <input name="content" type="text" />
+                    <p>{this.state.content}</p>
+                </form>
             </div>
         );
     }
